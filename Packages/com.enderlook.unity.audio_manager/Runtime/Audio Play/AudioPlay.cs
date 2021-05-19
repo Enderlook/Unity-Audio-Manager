db008ee0396bdc6b220e26491c9b6021afc42941
Enderlook <enderlook@gmail.com>
2021-05-19 19:27:34 -0300

    Refactor AudioPlay

--- Packages/com.enderlook.unity.audio_manager/Runtime/Audio Play/AudioPlay.cs
+++ Packages/com.enderlook.unity.audio_manager/Runtime/Audio Play/AudioPlay.cs
@@ -12,12 +12,6 @@ namespace Enderlook.Unity.AudioManager
     /// </summary>
     public partial struct AudioPlay
     {
-        private const bool hidePooledObjects = true;
-        private static int poolSize = 100;
-        private static Handle[] pool = new Handle[poolSize];
-        private static int poolIndex = -1;
-        private static int totalId;
-
         private readonly AudioFile audioFile;
         private Handle handle;
         private int generation; // If negative this contains other information described by constants named GENERATION_.
@@ -167,80 +161,6 @@ namespace Enderlook.Unity.AudioManager
                     handle.LoadMementoAndPlay(memento);
                 }
             }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static Handle GetOrCreateHandle()
-        {
-            if (poolIndex >= 0)
-            {
-                Handle handle = pool[poolIndex--];
-                if (handle != null)
-                {
-                    GameObject gameObject = handle.gameObject;
-                    gameObject.SetActive(true);
-#if UNITY_EDITOR
-                    if (hidePooledObjects)
-                        gameObject.hideFlags = HideFlags.None;
-#endif
-                    return handle;
-                }
-            }
-
-            return SlowPath();
-
-            //[MethodImpl(MethodImplOptions.NoInlining)] // TODO: Add this in C# 9
-            Handle SlowPath()
-            {
-                Handle handle;
-                while (true)
-                {
-                    if (poolIndex >= 0)
-                    {
-                        handle = pool[poolIndex--];
-                        if (handle != null)
-                        {
-                            GameObject gameObject = handle.gameObject;
-                            gameObject.SetActive(true);
-#if UNITY_EDITOR
-                            if (hidePooledObjects)
-                                gameObject.hideFlags = HideFlags.None;
-#endif
-                            break;
-                        }
-                    }
-                    else
-                    {
-#if UNITY_EDITOR
-                        GameObject gameObject = new GameObject($"Enderlook.Unity.AudioManager AudioSource ({totalId++})");
-#else
-                        GameObject gameObject = new GameObject();
-#endif
-                        handle = gameObject.AddComponent<Handle>();
-                        break;
-                    }
-                }
-                return handle;
-            }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void Return(Handle handle)
-        {
-            int index = poolIndex + 1;
-            if (index < PoolSize)
-            {
-                GameObject gameObject = handle.gameObject;
-                gameObject.SetActive(false);
-#if UNITY_EDITOR
-                if (hidePooledObjects)
-                    gameObject.hideFlags = HideFlags.HideInHierarchy;
-#endif
-                pool[index] = handle;
-                poolIndex = index;
-            }
-            else
-                UnityObject.Destroy(handle.gameObject);
         }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
